5211ea63ae1b5df826c225c627588da3d5559454
mohammad kaif <11249m018@kanchiuniv.ac.in>
2025-10-05 23:29:24 -0500

    Create constructor.cpp

---
+++ constructor.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+class student {
+    string name;
+    int age;
+
+public:
+
+
+    student() {
+        name = "Unknown";
+        age = 0;
+        cout << "Default constructor called" << endl;
+    }
+
+
+    
+    student(string n, int a) {
+        name = n;
+        age = a;
+        cout << "Parameterized constructor called" << endl;
+    }
+
+
+    
+    student(const student& s) {
+        name = s.name;
+        age = s.age;
+        cout << "Copy constructor called" << endl;
+    }
+
+    
+    ~student() {
+        cout << "Destructor called for " << name << endl;
+    }
+
+
+    
+    void display() {
+        cout << "Name: " << name << ", Age: " << age << endl;
+    }
+};
+
+int main() {
+    student s1;                   
+    student s2("Alice", 20);       
+    student s3(s2);               
+
+    s1.display();
+    s2.display();
+    s3.display();
+
+    return 0;
+}
